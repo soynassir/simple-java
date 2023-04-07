b5fb325f4828fdf0ea942d3c9ca404817e68f8cf
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2023-04-07 22:50:30 +0100

    Create HelloWorld.java
    First program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// A program to prints Hello World message
+public class HelloWorld 
+{
+  public static void main( String[] args )
+  {
+    System.out.println("Hello World!");
+  } // end method main
+} // end Class HelloWorld
