338bbebf020927894478832e6764b32b34a4fc3c
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2023-04-18 16:01:51 +0100

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,10 @@
+public class Name {
+  
+  static void displayMessage(String name) {
+    System.out.printf("Hello %s!", name.toUpperCase());
+  }
+  
+  public static void main(String[] args) {
+    displayMessage("John Doe");
+  }
+}
